0ef83606b01ccdb1a91424dd0a32f4478f784204
wiraki <11855154+wiraki@users.noreply.github.com>
2024-04-19 16:05:23 +0200

    Initial commit of chezmoi after migrating from YADM

---
+++ private_dot_config/visidata/config.py
@@ -0,0 +1,7 @@
+# Colors
+options.color_default = "7 on 234"
+options.color_menu = "16 on 68"
+
+# Other
+options.clipboard_copy_cmd = 'xclip -selection clipboard'
+
---
+++ util/update_nvim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This is probably not automatable.
+# New releases are found at https://github.com/neovim/neovim/releases
+# anonical@protonmail.com should receive notifications when new releases are made.
+
+# From https://github.com/neovim/neovim/releases download AppImage.
+#
+# It is also possible to download the .deb from the same place and install with apt/nala
