57ef44e7f2fa5eae42cb588aace6f05bed8b96fc
wiraki <11855154+wiraki@users.noreply.github.com>
2024-05-17 13:46:16 +0200

    Better indentation in nvim

--- private_dot_config/nvim-kick/init.lua
+++ private_dot_config/nvim-kick/init.lua
@@ -190,6 +190,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Better indenting (prevents from losing visual selection upon indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
