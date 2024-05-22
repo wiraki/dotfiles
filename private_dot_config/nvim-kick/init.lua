7ab48a769841d8ced9fc79ba1ff159ff53209d05
wiraki <11855154+wiraki@users.noreply.github.com>
2024-05-22 17:47:37 +0200

    Neovim: added flash.nvim plugin with some custom parameters

--- private_dot_config/nvim-kick/init.lua
+++ private_dot_config/nvim-kick/init.lua
@@ -929,6 +929,36 @@ require("lazy").setup({
 		},
 	},
 
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				-- options used when flash is activated through
+				-- a regular search with `/` or `?`
+				search = {
+					-- when `true`, flash will be activated during regular search by default.
+					-- You can always toggle when searching with `require("flash").toggle()`
+					-- Check the keymap assigned to the togle below, in the keys table.
+					enabled = true,
+				},
+				char = {
+					jump_labels = true,
+				},
+			},
+		},
+
+    -- stylua: ignore
+    keys = {
+     -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+	},
+
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
 		config = function()
