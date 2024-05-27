e49219862061ccf1e0b7ac0abc797bad37ca1fb6
wiraki <11855154+wiraki@users.noreply.github.com>
2024-05-27 14:16:37 +0200

    neovim: Telescope: enable previewer for current buffer fuzzy find (leader+/)

--- private_dot_config/nvim-kick/init.lua
+++ private_dot_config/nvim-kick/init.lua
@@ -453,7 +453,7 @@ require("lazy").setup({
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 					winblend = 10,
-					previewer = false,
+					previewer = true,
 				}))
 			end, { desc = "[/] Fuzzily search in current buffer" })
 
