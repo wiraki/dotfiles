166ce05c7d3142795402d40bb3167452c1cbaea2
wiraki <11855154+wiraki@users.noreply.github.com>
2024-05-27 14:19:01 +0200

    neovim: flash: disable multi-line search with f,F,t,T operations. if searching multi-line we are using "/" instead

--- private_dot_config/nvim-kick/init.lua
+++ private_dot_config/nvim-kick/init.lua
@@ -946,15 +946,18 @@ require("lazy").setup({
 					-- Check the keymap assigned to the togle below, in the keys table.
 					enabled = true,
 				},
+				-- options used when flash is activated through
+				-- `f`, `F`, `t`, `T`, `;` and `,` motions
 				char = {
 					jump_labels = true,
+					multi_line = false,
 				},
 			},
 		},
 
     -- stylua: ignore
     keys = {
-     -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
