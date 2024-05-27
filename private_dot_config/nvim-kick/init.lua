b5d3be216cc7f21d0e562e6bdb9e1ed050b77074
wiraki <11855154+wiraki@users.noreply.github.com>
2024-05-27 14:15:42 +0200

    neovim: enable ctrl+j or ctrl+k to cycle through telescope results

--- private_dot_config/nvim-kick/init.lua
+++ private_dot_config/nvim-kick/init.lua
@@ -415,11 +415,14 @@ require("lazy").setup({
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+					},
+				},
 				-- pickers = {}
 				extensions = {
 					["ui-select"] = {
