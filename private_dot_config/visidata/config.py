025467a04d7812f9cee2129fa40a575572dc4c42
wiraki <11855154+wiraki@users.noreply.github.com>
2024-06-19 11:53:15 +0200

    Revert "visidata: automatically format float columns with 8 decimal places"
    This reverts commit c1c8c9863886c24ba5c7fec613c29078aa09a79c.

--- private_dot_config/visidata/config.py
+++ private_dot_config/visidata/config.py
@@ -3,5 +3,7 @@ options.color_default = "7 on 234"
 options.color_menu = "16 on 68"
 
 # Other
-options.clipboard_copy_cmd = 'xclip -selection clipboard'
+options.clipboard_copy_cmd = "xclip -selection clipboard"
 
+# Default float formatting
+options.disp_float_fmt = "{:.08f}"
