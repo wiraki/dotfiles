da621012fbe25b0de4762fc4a936a562ed78011e
wiraki <11855154+wiraki@users.noreply.github.com>
2024-09-10 14:30:56 +0200

    yazi: set a default linemode to show size and mtime

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -22,3 +22,18 @@ require("relative-motions"):setup({
 	show_numbers = "relative",
 	show_motion = true,
 })
+
+-- Custome linemode to show both size and mtime
+function Linemode:size_and_mtime()
+	local year = os.date("%Y")
+	local time = math.floor(self._file.cha.modified or 0)
+
+	if time > 0 and os.date("%Y", time) == year then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = time and os.date("%b %d  %Y", time) or ""
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size) or "", time))
+end
