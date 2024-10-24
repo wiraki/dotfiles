94e3dbd94fb309fb4f70e34a37a1d1d38efb48be
wiraki <11855154+wiraki@users.noreply.github.com>
2024-10-24 15:38:49 +0200

    neovim: allow hidden files in telescope find_files

--- private_dot_config/nvim-kick/init.lua
+++ private_dot_config/nvim-kick/init.lua
@@ -442,7 +442,11 @@ require("lazy").setup({
 						},
 					},
 				},
-				-- pickers = {}
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
