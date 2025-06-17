bd9361fed2866645ebb31e3acb495e949b6c1e66
wiraki <11855154+wiraki@users.noreply.github.com>
2025-06-17 14:14:52 +0200

    yazi: re-enable yatline as it seems issues have been fixed

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -34,67 +34,67 @@ end
 
 -- yatline (https://github.com/imsi32/yatline.yazi)
 -- using yatline-catppuccin (https://github.com/imsi32/yatline-catppuccin.yazi)
--- local catppuccin_theme = require("yatline-catppuccin"):setup("macchiato") -- or "latte" | "frappe" | "macchiato"
--- require("yatline"):setup({
--- 	-- ===
--- 	section_separator = { open = "", close = "" },
--- 	part_separator = { open = "", close = "" },
--- 	inverse_separator = { open = "", close = "" },
---
--- 	tab_width = 20,
--- 	tab_use_inverse = false,
---
--- 	display_header_line = true,
--- 	display_status_line = true,
---
--- 	header_line = {
--- 		left = {
--- 			section_a = {
--- 				{ type = "line", custom = false, name = "tabs", params = { "left" } },
--- 			},
--- 			section_b = {},
--- 			section_c = {},
--- 		},
--- 		right = {
--- 			section_a = {
--- 				{ type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
--- 			},
--- 			section_b = {
--- 				{ type = "string", custom = false, name = "date", params = { "%X" } },
--- 			},
--- 			section_c = {},
--- 		},
--- 	},
---
--- 	status_line = {
--- 		left = {
--- 			section_a = {
--- 				{ type = "string", custom = false, name = "tab_mode" },
--- 			},
--- 			section_b = {
--- 				{ type = "string", custom = false, name = "hovered_size" },
--- 			},
--- 			section_c = {
--- 				{ type = "string", custom = false, name = "hovered_name" },
--- 				{ type = "coloreds", custom = false, name = "count" },
--- 			},
--- 		},
--- 		right = {
--- 			section_a = {
--- 				{ type = "string", custom = false, name = "cursor_position" },
--- 			},
--- 			section_b = {
--- 				{ type = "string", custom = false, name = "cursor_percentage" },
--- 			},
--- 			section_c = {
--- 				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
--- 				{ type = "coloreds", custom = false, name = "permissions" },
--- 			},
--- 		},
--- 	},
--- 	-- ===
--- 	theme = catppuccin_theme,
--- })
+local catppuccin_theme = require("yatline-catppuccin"):setup("macchiato") -- or "latte" | "frappe" | "macchiato"
+require("yatline"):setup({
+	-- ===
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+
+	tab_width = 20,
+	tab_use_inverse = false,
+
+	display_header_line = true,
+	display_status_line = true,
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
+			},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "date", params = { "%X" } },
+			},
+			section_c = {},
+		},
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_name" },
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+	-- ===
+	theme = catppuccin_theme,
+})
 
 -- Customize searchjump plugin colors to catppuccin
 require("searchjump"):setup({
